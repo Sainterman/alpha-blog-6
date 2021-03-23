0663242f3aa89cd5be3ad7229574bf30acfa0af4
Santiago Castillo <sancastillog@gmail.com>
2021-03-23 09:43:44 -0500

    Create categories, functional test for category creation

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,28 @@
+class CategoriesController < ApplicationController
+    def index
+    
+    end
+    
+    def show
+    
+    end
+
+    def new
+        @category = Category.new
+    end
+    
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:notice] = "category succesfully created"
+            redirect_to @category
+        else
+            render 'new'
+        end
+    end
+
+    private
+    def category_params
+        params.require(:category).permit(:name)
+    end
+end
