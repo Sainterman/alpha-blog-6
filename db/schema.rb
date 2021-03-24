f5ff147016d18d50b2b1739d40742f983891aed8
Santiago Castillo <sancastillog@gmail.com>
2021-03-24 10:57:18 -0500

    Categories index, show and update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_18_220450) do
+ActiveRecord::Schema.define(version: 2021_03_23_170720) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
